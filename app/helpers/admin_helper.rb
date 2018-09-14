0d8baafc9f946b0a46b2950e43402a33d1aaa6f2
Thomas Sandu <t_sandu@yahoo.com>
2018-09-14 09:02:08 +0300

    partial für Verfügbarkeiten gemacht

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -9,13 +9,4 @@ module AdminHelper
     week_start.strftime( "%m/%d/%y" ) + ' - ' + week_end.strftime(
     "%m/%d/%y" )
   end
-
-  def finde_mo_bis_so_datum (week_num)
-    datums = []
-    year = Time.now.year
-    (1..7).each do |day|
-      datums=+ Date.commercial(year, week_num, day)
-    end
-    datums
-  end
 end
